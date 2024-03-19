b61f6dd4ee2860935743ca2b42cd376b7b7f1550
gagan2125 <gagansk2125@gmail.com>
2024-03-19 19:37:36 +0530

    clerk webhook updated

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,4 +1,5 @@
 import mongoose, { Mongoose } from "mongoose";
+
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
@@ -17,12 +18,17 @@ if (!cached) {
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
+
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(MONGODB_URL, { dbName: "canvai", bufferCommands: false });
+    mongoose.connect(MONGODB_URL, {
+      dbName: "canvai",
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/webhooks/clerk"],
-  ignoredRoutes: ["/no-auth-in-this-route"],
 });
 
 export const config = {
